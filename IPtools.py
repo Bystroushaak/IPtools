59691fcec0a2fb2a86d3390c538ecc3128347c84
Bystroushaak <bystrousak@kitakitsune.org>
2013-07-23 23:55:40 +0200

    Version 3.0 - brutally refactored and cleaned. getIP() now uses http://www.whatsmyip.us/showipsimple.php sshTunnel() now actually works how it should from the beginning.

--- IPtools.py
+++ IPtools.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# IPtools v2.2.0 (07.07.2012) by Bystroushaak (bystrousak@kitakitsune.org)
+# IPtools v3.0.0 (23.07.2013) by Bystroushaak (bystrousak@kitakitsune.org)
 #
-# Imports ======================================================================
-import os
+# Imports #####################################################################
 import sys
 import socket
 import random
 import urllib2
-from threading import Timer
 
 
 try:
@@ -32,14 +30,15 @@ from timeout import timeout
 
 
 
-# Vars =========================================================================
-IP = ["", ""]
-TIMEOUT = 5.0
-ORIG_SOCK = None
+# Vars ########################################################################
+__IP         = ["", ""]
+TIMEOUT      = 10.0
+ORIG_SOCK    = None
+EXPECT_CLASS = []  # this is necessary due to garbage collector
 
 
 
-# Functions & objects ==========================================================
+# Functions & objects #########################################################
 class ProxyException(Exception):
 	def __init__(self, msg):
 		self.msg = msg
@@ -52,37 +51,33 @@ class ProxyTimeoutException(ProxyException):
 		return repr(self.msg)
 
 
-@timeout(int(TIMEOUT), None)
+@timeout(
+	int(TIMEOUT / 2),
+	"This proxy is slower than your " + str(TIMEOUT) + "s .TIMEOUT property!"
+)
 def __getPage(url):
-	def handler():
-		raise ProxyTimeoutException("This proxy is fucking slow, slower than your " + str(TIMEOUT) + "s timeout (see TIMEOUT property).")
-
-	t = Timer(TIMEOUT, handler)
-	t.start()
-
-	f = urllib2.urlopen(url, None, 2.5)
+	f = urllib2.urlopen(url)
 	data = f.read()
-	t.cancel()
-
 	f.close()
+
 	return data
 
 
 def getIP():
-	"Return IP address (from http://anoncheck.security-portal.cz/)."
+	"Return current IP address."
 
-	ipstr = filter(lambda x: x.startswith("Vaše IP"), __getPage("http://anoncheck.security-portal.cz/").splitlines())[0]
-	return "".join(filter(lambda x: x.isdigit() or x == ".", list(ipstr)))
-	
+	data = __getPage("http://www.whatsmyip.us/showipsimple.php")
+	return data.replace('document.write("', "").replace('");', "").strip()
 
-@timeout(int(TIMEOUT * 2), None)
+
+@timeout(int(TIMEOUT), None)
 def installProxy(SOCK_ADDR, SOCK_PORT, check_ip = True):
 	"Install SOCKS5 proxy."
 
 	# get normal ip
 	if check_ip:
 		try:
-			IP[0] = getIP()
+			__IP[0] = getIP()
 		except:
 			raise ProxyException("Can't connect to internet!")
 
@@ -97,20 +92,40 @@ def installProxy(SOCK_ADDR, SOCK_PORT, check_ip = True):
 	# get ip over proxy
 	if check_ip:
 		try:
-			IP[1] = getIP()
+			__IP[1] = getIP()
 		except ProxyTimeoutException, e:
 			raise e
 		except:
-			raise ProxyException("Your SOCK5 proxy (" + SOCK_ADDR + ":" + str(SOCK_PORT) + ") isn't responding!")
+			raise ProxyException(
+				"Your SOCK5 proxy (" +
+				SOCK_ADDR +
+				":" +
+				str(SOCK_PORT) +
+				") isn't responding!"
+			)
 
-		if IP[0] == IP[1]:
+		if __IP[0] == __IP[1]:
 			raise ProxyException("This proxy doesn't hides your IP, use better one")
 
 
-def sshTunnel(login_serv, e, port = None, check_ip = True, timeout = 10):
-	"Create ssh SOCKS5 tunnel."
+def sshTunnel(login_serv, e, port = None, check_ip = True, timeout = TIMEOUT):
+	"""
+	Create (but don't use -> see installProxy()) SOCKS5 tunnel over SSH.
 
-	if port == None:
+	Example:
+	---
+	print IPtools.getIP() # -> your IP
+
+	proxy_port = IPtools.sshTunnel("ssh@somewhere.com", timeout = 30)
+	IPtools.installProxy("localhost", 2222)
+
+	print IPtools.getIP() # -> tunnel's IP
+	---
+
+	Return port where tunnel is listenning.
+	"""
+
+	if port is None:
 		port = random.randint(1025, 65534)
 
 	# create ssh tunnel
@@ -122,11 +137,12 @@ def sshTunnel(login_serv, e, port = None, check_ip = True, timeout = 10):
 		c.sendline("yes")
 		c.expect(e, timeout = timeout)
 
-	installProxy("127.0.0.1", port, check_ip)
+	EXPECT_CLASS.append(c)  # dont let garbage collector delete this
 
-	return c
+	return port
 
 
-# Main =========================================================================
+
+# Main ########################################################################
 if __name__ == '__main__':
 	print "IPTools; https://github.com/Bystroushaak/IPtools"
