5025e0efe241f52abe8ebb873adc3dbc2aa4b49b
Bystroushaak <bystrousak@kitakitsune.org>
2013-09-24 10:24:35 +0200

    Refactored little bit, added few docstrings, repaired bug in restoring original socket and added restoreSocket() function.

--- IPtools.py
+++ IPtools.py
@@ -1,10 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# IPtools v3.0.1 (24.07.2013) by Bystroushaak (bystrousak@kitakitsune.org)
+"""
+Module to ease usage of SOCK5 proxy and SSH tunnel installation.
+
+There is also functions for checking your IP.
+
+Examples:
+--
+>>> import IPtools
+>>> IPtools.getIP()
+'429.292.637.2'  # modified to protect my privacy
+--
+
+SSH proxy:
+--
+>>> port = IPtools.sshTunnel("bystrousak@kitakitsune.org", "###")
+>>> IPtools.installProxy("localhost", port)
+>>> IPtools.getIP()
+'31.31.73.113'
+--
+
+Restoring original socket:
+--
+>>> import IPtools
+>>> IPtools.getIP()
+'429.292.637.2'
+>>> IPtools.installProxy("localhost", IPtools.sshTunnel("bystrousak@kitakitsune.org", "###"))
+>>> IPtools.getIP()
+'31.31.73.113'
+>>> IPtools.restoreSocket()
+>>> IPtools.getIP()
+'429.292.637.2'
+>>>
+--
+
+Author: Bystroushaak (bystrousak@kitakitsune.org)
+"""
 #
 # Imports #####################################################################
 import sys
+import copy
 import socket
 import random
 import urllib2
@@ -13,17 +48,22 @@ import urllib2
 try:
 	import pexpect
 except ImportError, e:
-	sys.stderr.write("I do require pexpect module. You can get it from\n")
-	sys.stderr.write("http://sourceforge.net/projects/pexpect/\n")
-	raise e
+	sys.stderr.write(
+		"I do require pexpect module. You can get it from\n"
+		"http://sourceforge.net/projects/pexpect/ OR from 'python-pexpect' ubuntu"
+		"package OR from PIP (pexpect)\n"
+	)
+	raise
 
 
 try:
 	import socks
 except ImportError, e:
-	sys.stderr.write("I do require sock module. You can get it from\n")
-	sys.stderr.write("http://socksipy.sourceforge.net/\n")
-	raise e
+	sys.stderr.write(
+		"I do require sock module. You can get it from\n"
+		"http://socksipy.sourceforge.net/ OR from PIP (SocksiPy)\n"
+	)
+	raise
 
 
 from timeout import timeout
@@ -42,17 +82,15 @@ EXPECT_CLASS = []  # this is necessary due to garbage collector
 class ProxyException(Exception):
 	def __init__(self, msg):
 		self.msg = msg
+
 	def __str__(self):
 		return repr(self.msg)
-class ProxyTimeoutException(ProxyException):
-	def __init__(self, msg):
-		self.msg = msg
-	def __str__(self):
-		return repr(self.msg)
+
 
 
 @timeout(
 	int(TIMEOUT / 2),
+	None,
 	"This proxy is slower than your " + str(TIMEOUT) + "s .TIMEOUT property!"
 )
 def __getPage(url):
@@ -72,18 +110,22 @@ def getIP():
 
 @timeout(int(TIMEOUT), None)
 def installProxy(SOCK_ADDR, SOCK_PORT, check_ip = True):
-	"Install SOCKS5 proxy."
+	"""
+	Install SOCKS5 proxy.
+
+	Raise ProxyTimeoutException
+	"""
 
 	# get normal ip
 	if check_ip:
 		try:
 			__IP[0] = getIP()
-		except:
-			raise ProxyException("Can't connect to internet!")
+		except Exception, e:
+			raise ProxyException("Can't connect to internet!\n" + str(e))
 
 	# save original socket
 	global ORIG_SOCK
-	ORIG_SOCK = socket.socket
+	ORIG_SOCK = copy.copy(socket.socket)
 
 	# apply proxy
 	socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, SOCK_ADDR, SOCK_PORT)
@@ -93,22 +135,18 @@ def installProxy(SOCK_ADDR, SOCK_PORT, check_ip = True):
 	if check_ip:
 		try:
 			__IP[1] = getIP()
-		except ProxyTimeoutException, e:
-			raise e
-		except:
+		except Exception, e:
 			raise ProxyException(
-				"Your SOCK5 proxy (" +
-				SOCK_ADDR +
-				":" +
-				str(SOCK_PORT) +
-				") isn't responding!"
+				"Your SOCK5 proxy (" + SOCK_ADDR + ":" + str(SOCK_PORT) + ") "
+				"isn't responding!\n" +
+				str(e)
 			)
 
 		if __IP[0] == __IP[1]:
 			raise ProxyException("This proxy doesn't hides your IP, use better one")
 
 
-def sshTunnel(login_serv, e, port = None, check_ip = True, timeout = TIMEOUT):
+def sshTunnel(login_serv, e, port = None, timeout = TIMEOUT):
 	"""
 	Create (but don't use -> see installProxy()) SOCKS5 tunnel over SSH.
 
@@ -126,7 +164,7 @@ def sshTunnel(login_serv, e, port = None, check_ip = True, timeout = TIMEOUT):
 	print IPtools.getIP() # -> tunnel's IP
 	---
 
-	Return port where tunnel is listenning.
+	Return port where the tunnel is listenning.
 	"""
 
 	if port is None:
@@ -146,7 +184,7 @@ def sshTunnel(login_serv, e, port = None, check_ip = True, timeout = TIMEOUT):
 	return port
 
 
-
-# Main ########################################################################
-if __name__ == '__main__':
-	print "IPTools; https://github.com/Bystroushaak/IPtools"
+def restoreSocket():
+	"""Removes proxy from your system and restores original socket."""
+	global ORIG_SOCK
+	socket.socket = ORIG_SOCK
